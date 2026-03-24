96c36e7fff93d449fe17e62398729599f9e38aca
sarthak-varshney-dv <sarthak.varshney.dv@gmail.com>
2026-03-24 21:15:32 +0400

    drive item added

---
+++ Aligner_Verification/sv_algn_apb_pkg/sv_algn_item_drv.sv
@@ -0,0 +1,41 @@
+`ifndef SV_APB_ITEM_DRV_SV
+ `define SV_APB_ITEM_DRV_SV
+
+class sv_apb_item_drv extends sv_apb_item_base;
+ 
+  rand int unsigned pre_drive_delay;
+  
+  rand int unsigned post_drive_delay;
+  
+  constraint pre_drive_delay_default {
+ soft pre_drive_delay<5;
+  }
+    
+  constraint post_drive_delay_default {
+  soft post_drive_delay<5;
+  }
+
+  
+  `uvm_object_utils(sv_apb_item_drv)
+  
+  function new(string name="");
+    super.new(name);
+  endfunction
+  
+  virtual function string convert2string();
+    string result=super.convert2string();
+    
+    if(dir==sv_apb_write) begin
+      result = $sformatf(" %0s , Data: %0x",result, data);
+    end
+    
+    result = $sformatf(" %0s ,pre_drive_delay : %0d , post_drive_delay: %0d ",result,pre_drive_delay,post_drive_delay );
+    
+    return result;
+  endfunction
+endclass
+
+
+
+
+`endif
