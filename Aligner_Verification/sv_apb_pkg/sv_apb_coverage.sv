73571d037c160bf18d2c67fc52b78ff38abcb952
sarthak-varshney-dv <sarthak.varshney.dv@gmail.com>
2026-03-26 23:14:14 +0400

    logic to sample the index toggeling done

--- Aligner_Verification/sv_apb_pkg/sv_apb_coverage.sv
+++ Aligner_Verification/sv_apb_pkg/sv_apb_coverage.sv
@@ -34,8 +34,22 @@
 
  class sv_apb_coverage extends uvm_component;
   
+  //port to connect from monitor
   uvm_analysis_imp_item#(sv_apb_item_mon,sv_apb_coverage) port_item;
 
+  //index wraper objects to sample toggling of each index 
+  sv_apb_cover_index_wrapper#(`SV_APB_MAX_ADDR_WIDTH)  wrap_cover_addr_0; 
+
+  sv_apb_cover_index_wrapper#(`SV_APB_MAX_ADDR_WIDTH)  wrap_cover_addr_1;
+
+  sv_apb_cover_index_wrapper#(`SV_APB_MAX_DATA_WIDTH)  wrap_cover_pwdata_0;
+
+  sv_apb_cover_index_wrapper#(`SV_APB_MAX_DATA_WIDTH)  wrap_cover_pwdata_1;
+
+  sv_apb_cover_index_wrapper#(`SV_APB_MAX_DATA_WIDTH)  wrap_cover_prdata_0;
+
+  sv_apb_cover_index_wrapper#(`SV_APB_MAX_DATA_WIDTH)  wrap_cover_prdata_1; 
+  
   `uvm_component_utils(sv_apb_coverage)
 
   covergroup cover_item with function sample(sv_apb_item_mon item);
@@ -53,9 +67,10 @@
     option.comment="length of APB_ACCESS"
 
     bins equal_2 = {2}; //shortest item possible
-    bins less_than_ten = {[3:10]};
+    bins less_than_ten[8] = {[3:10]};
     bins greater_than_ten= {[10:$]};
   }
+
 
   prev_item_delay : coverpoint item.prev_item_delay {
     option.comment= "delay prior to APB_ACCESS"
@@ -72,16 +87,66 @@
     super.new(name,parent);
         cover_item=new();
         cover_item.set_inst_name($sformatf("%s_%s",get_full_name(),"cover_item"));
+
+        wrap_cover_addr_0   = sv_apb_cover_index_wrapper#(`SV_APB_MAX_ADDR_WIDTH)::type_id::create("wrap_cover_addr_0");
+        wrap_cover_addr_1   = sv_apb_cover_index_wrapper#(`SV_APB_MAX_ADDR_WIDTH)::type_id::create("wrap_cover_addr_1"); 
+        wrap_cover_pwdata_0 = sv_apb_cover_index_wrapper#(`SV_APB_MAX_DATA_WIDTH)::type_id::create("wrap_cover_pwdata_0");
+        wrap_cover_pwdata_1 = sv_apb_cover_index_wrapper#(`SV_APB_MAX_DATA_WIDTH)::type_id::create("wrap_cover_pwdata_1"); 
+        wrap_cover_prdata_0 = sv_apb_cover_index_wrapper#(`SV_APB_MAX_DATA_WIDTH)::type_id::create("wrap_cover_prdata_0");
+        wrap_cover_prdata_1 = sv_apb_cover_index_wrapper#(`SV_APB_MAX_DATA_WIDTH)::type_id::create("wrap_cover_prdata_1"); 
+
+
     endfunction 
- endclass 
 
- 
-
+    
  virtual function void write_item(sv_apb_item_mon item);
   
   cover_item.sample(item);
+   
+ //sampling of address index toggling
+ for(int i=0;i<`SV_APB_MAX_ADDR_WIDTH;i++) begin
+    if(item.addr[i]) begin
+    wrap_cover_addr_1.sample(i);
+    end
+    else begin
+        wrap_cover_addr_0.sample(i);
+ end
+ end
 
+ //sampling of data index toggiling
+ for(int i=0;i<`SV_APB_MAX_DATA_WIDTH,i++) begin
+    
+    case(item.dir)
+       SV_APB_WRITE : begin
+    if(item.pwdata[i]) begin
+    wrap_cover_pwdata_1.sample(i);
+          end
+    else begin
+        wrap_cover_pwdata_0.sample(i);
+          end
+       end
+
+    SV_APB_READ : begin
+    if(item.prdata[i]) begin
+    wrap_cover_prdata_1.sample(i);
+          end
+    else begin
+        wrap_cover_prdata_0.sample(i);
+          end
+     end
+     default : begin
+        `uvm_error("ALOGRITHM ISSUE",$sformatf("item.dir:%0s not supported",item.dir.name()))
+     end
+    endcase
+    
+
+ end
 
  endfunction
+
+
+ endclass 
+
+ 
 
  `endif
