81f2e8711e089024a954c8c97541b0f66cab9032
sarthak-varshney-dv <sarthak.varshney.dv@gmail.com>
2026-03-28 17:31:28 +0400

    md rx and tx interface connections done

--- Aligner_Verification/sv_md_pkg/sv_md_interface.sv
+++ Aligner_Verification/sv_md_pkg/sv_md_interface.sv
@@ -1,11 +1,11 @@
 `ifndef SV_MD_INTERFACE_SV
  `define SV_MD_INTERFACE_SV
 
-interface sv_md_if#(int unsigned  data_width = 32) (input clk);
+interface sv_md_interface#(int unsigned  DATA_WIDTH = 32) (input clk);
 
- localparam OFFSET_WIDTH = ;
+ localparam OFFSET_WIDTH = $clog2(DATA_WIDTH / 8) > 1 ? $clog2(data_width / 8) : 1  ;
 
- localparam SIZE_WIDTH = ;
+ localparam SIZE_WIDTH =   $clog2(DATA_WIDTH / 8) + 1 ;
 
  logic reset_n;
 
@@ -15,13 +15,11 @@ interface sv_md_if#(int unsigned  data_width = 32) (input clk);
 
  logic [OFFSET_WIDTH-1 : 0] offset ;
 
- logic [data_width - 1 : 0] data ;
+ logic [DATA_WIDTH - 1 : 0] data ;
 
  logic {SIZE_WIDTH -1 : 0 } size ;
 
  logic err ; 
-
-
 
 
 
--- Aligner_Verification/testbench.sv
+++ Aligner_Verification/testbench.sv
@@ -26,11 +26,18 @@ module testbench();
     apb_if.preset_n=1;
   end
   
-  //INTERFACE HANDLE
+  //APB INTERFACE HANDLE
     sv_apb_interface apb_if(.pclk(clk));
 
+  //MD RX INTERFACE HANDLE
+    sv_md_interface#(32) md_rx_if(.clk(clk)) ;
+
+  //MD TX INTERFACE HANDLE
+    sv_md_interface#(32) md_tx_if(.clk(clk)) ;
   
-  
+
+  assign md_rx_if.reset_n = apb_if.preset_n ;
+  assign md_tx_if.reset_n = apb_if.preset_n ;
   
   initial begin
     $dumpfile("dump.vcd");
@@ -40,7 +47,9 @@ module testbench();
   uvm_config_db#(virtual sv_apb_interface)::set(null,"uvm_test_top.env.apb_agent","vif",apb_if);
   //when instantiate apb_agent, use name apb_agent 
     
-    
+  uvm_config_db#(virtual md_rx_if#(32))::set(null,"uvm_test_top.env.md_rx_agent","vif",md_rx_if);
+
+  uvm_config_db#(virtual md_tx_if#(32))::set(null,"uvm_test_top.env.md_tx_agent","vif",md_tx_if);
     
     run_test("  ");
   end
@@ -56,8 +65,21 @@ module testbench();
     .pwdata(apb_if.pwdata),
     .pready(apb_if.pready),
     .prdata(apb_if.prdata),
-    .pslverr(apb_if.pslverr)
+    .pslverr(apb_if.pslverr),
+
+    .md_rx_valid(md_rx_if.valid),
+    .md_rx_ready(md_rx_if.ready),
+    .md_rx_size(md_rx_if.size),
+    .md_rx_data(md_rx_if.data),
+    .md_rx_offset(md_rx_if.offset),
+    .md_rx_err(md_rx_if.err),
     
+    .md_tx_valid(md_tx_if.valid),
+    .md_tx_ready(md_tx_if.ready),
+    .md_tx_size(md_tx_if.size),
+    .md_tx_data(md_tx_if.data),
+    .md_tx_offset(md_tx_if.offset),
+    .md_tx_err(md_tx_if.err)
   );
   
   
