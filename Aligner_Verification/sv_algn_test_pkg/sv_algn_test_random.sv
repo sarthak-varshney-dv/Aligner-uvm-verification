8873dcbc7870eb62dcdb8c09592729da0cffb95e
sarthak-varshney-dv <sarthak.varshney.dv@gmail.com>
2026-03-30 01:58:02 +0400

    random test created for md rx interface

--- Aligner_Verification/sv_algn_test_pkg/sv_algn_test_random.sv
+++ Aligner_Verification/sv_algn_test_pkg/sv_algn_test_random.sv
@@ -10,7 +10,27 @@ class sv_algn_test_random extends sv_algn_test_base;
       
    endfunction
   
-  
+  virtual task run_phase(uvm_phase phase);
+ 
+  phase.raise_objection(this."test_done");
+
+  #(100ns);
+   begin
+      sv_md_sequence_base_master seq= sv_md_sequence_base_master::type_id::create("seq");
+      seq.set_sequencer(env.md_rx_agent.sequencer);
+
+      repeat(10) begin
+         void'(randomize(seq));
+
+      seq.start(env.md_rx_agent.sequencer);
+
+      end
+   end
+
+  phase.drop_objection(this."test_done");
+
+
+  endtask
 endclass
 
 `endif
--- Aligner_Verification/sv_algn_test_pkg/sv_algn_test_reg_access.sv
+++ Aligner_Verification/sv_algn_test_pkg/sv_algn_test_reg_access.sv
@@ -1,23 +1,16 @@
-`ifndef SV_ALGN_TEST_BASE_SV
- `define SV_ALGN_TEST_BASE_SV
+`ifndef SV_ALGN_TEST_REG_ACCESS_SV
+ `define SV_ALGN_TEST_REG_ACCESS_SV
 
-class sv_algn_test_base extends uvm_test;
+class sv_algn_test_reg_access extends uvm_test;
   
-  // Environment handler
-   sv_algn_env env ;
-  `uvm_component_utils(sv_algn_test_base)
+ 
+  `uvm_component_utils(sv_algn_test_reg_access)
   
   function new(string name="" , uvm_component parent);
       super.new(name,parent);
       
    endfunction
   
-  virtual function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    
-    env = sv_algn_env::type_id::create("env",this);
-    
-   endfunction
     
   virtual task run_phase(uvm_phase phase);
  phase.raise_objection(this,"test_done");
