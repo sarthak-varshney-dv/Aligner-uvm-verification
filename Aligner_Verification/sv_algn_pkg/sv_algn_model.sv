2ab3d3a6a31d95ca2405b9088c85258dde324b22
sarthak-varshney-dv <sarthak.varshney.dv@gmail.com>
2026-04-05 21:03:01 +0400

    Model file created.Reg_block instantiated and locked

---
+++ Aligner_Verification/sv_algn_pkg/sv_algn_model.sv
@@ -0,0 +1,31 @@
+`ifndef SV_ALGN_MODEL_SV
+ `define SV_ALGN_MODEL_SV
+
+class sv_algn_model extends uvm_component 
+
+sv_algn_reg_block reg_block ;
+
+`uvm_component_utils(sv_algn_model)
+
+function new(string name = "");
+  super.new(name);
+endfunction
+
+virtual function void build_phase(uvm_phase phase)
+super.build_phase(phase);
+
+if(reg_block == null) begin
+    reg_block = sv_algn_reg_block::type_id::create("reg_block");
+
+    
+reg_block.build();
+reg_block.lock_model();
+
+
+end
+
+
+endfunction
+endclass
+
+ `endif 
