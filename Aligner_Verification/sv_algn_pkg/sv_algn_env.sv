74c10b2c1169bdcee952d90110256b468b23ea14
sarthak-varshney-dv <sarthak.varshney.dv@gmail.com>
2026-04-06 01:46:28 +0400

    predictor connected in the environment

--- Aligner_Verification/sv_algn_pkg/sv_algn_env.sv
+++ Aligner_Verification/sv_algn_pkg/sv_algn_env.sv
@@ -8,6 +8,8 @@ class sv_algn_env extends uvm_env;
   sv_md_agent_master#(32) md_rx_agent;
 
   sv_md_agent_slave#(32) md_tx_agent;
+
+  sv_algn_reg_predictor#(sv_apb_item_mon) predictor ;
   
   `uvm_component_utils(sv_algn_env)
   
@@ -23,13 +25,22 @@ class sv_algn_env extends uvm_env;
 
     md_rx_agent= sv_md_agent_master#(32)::type_id::create("md_rx_agent",this);
     md_tx_agent= sv_md_agent_slave#(32)::type_id::create("md_tx_agent",this);
+
+    predictor = sv_algn_reg_predictor#(sv_apb_item_mon)::type_id::create("predictor",this);
     
   endfunction
 
   virtual function void connect_phase(uvm_phase phase);
   super.connect_phase(phase);
   
-  
+  sv_apb_reg_adapter adapter= sv_apb_reg_adapter::type_id::create("adapter");;
+
+  predictor.adapter = adapter;
+
+  predictor.map = model.reg_block.default_map ;
+
+  apb_agent.output_port.connect(predictor.bus_in);
+
 
   endfunction
   
